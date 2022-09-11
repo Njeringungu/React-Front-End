f3bfc276b0701e83193e8b9fdb5876da09af0d63
Njeri Ngungu <janetnjerin89@gmail.com>
2022-09-11 16:47:09 +0300

    style appoitment form

--- src/Components/Appointment.js
+++ src/Components/Appointment.js
@@ -28,46 +28,44 @@ function Appointment(){
       }
 
 return(
-    <>
+    <div className="appointment">
     
-  <h1>Book<em>!</em></h1>
+      <h1>Book<em>!</em></h1>
+      <div className="formcard">
+        <form className="form" onSubmit={handleSubmit}>
 
-  <form className="form" onSubmit={handleSubmit}>
+            <label htmlFor="name">Patients Id:</label>
+            <input type="number" id="patient_id" value = {appointments.patient_id} onChange={ ( e ) => handleChange( e ) }/>
 
-  <label htmlFor="name">Patients Id:</label>
-    <input type="number" id="patient_id" value = {appointments.patient_id} onChange={ ( e ) => handleChange( e ) }/>
+            <label htmlFor="name">Doctors Id:</label>
+            <input type="number" id="doctor_id" value = {appointments.doctor_id} onChange={ ( e ) => handleChange( e ) }/>
 
-  <label htmlFor="name">Doctors Id:</label>
-    <input type="number" id="doctor_id" value = {appointments.doctor_id} onChange={ ( e ) => handleChange( e ) }/>
+            <label htmlFor="date">Date</label>
+            <input type="date" id="date" value={appointments.date} onChange = { ( e ) => handleChange( e ) } />
+            <input type="submit" value="Submit" />
 
-    <label htmlFor="date">Date</label>
-        <input type="date" id="date" value={appointments.date} onChange = { ( e ) => handleChange( e ) } />
-
-<input type="submit" value="Submit" />
-  </form>
-  
-  <card className="doctorCard">
-    <h2>Our Doctors</h2>
-        <li>Id:1,
-          <br/>Name:Michael Wafula,<br/>
-        Speciaulty:Dentist</li>
-        
-        <li>Id:2,
-          <br/>Name:Lucy Atieno,<br/>
-        Speciaulty: Dermatologist</li>
-        
-        <li> Id:3,
-          <br/>Name: Jane Wangui,<br/>
-        Speciaulty: ENT</li>
-      
-        <li>Id:4,<br/>
-        Name:James Mogaka,<br/>
-        Speciaulty:Optician</li>
-       
-       
-        
-  </card>
-  
-  </>
+        </form>
+    
+        <card className="doctorCard">
+          <h2>Our Doctors</h2>
+              <li>Id:1,
+                <br/>Name:Michael Wafula,<br/>
+              Speciaulty:Dentist</li>
+              
+              <li>Id:2,
+                <br/>Name:Lucy Atieno,<br/>
+              Speciaulty: Dermatologist</li>
+              
+              <li> Id:3,
+                <br/>Name: Jane Wangui,<br/>
+              Speciaulty: ENT</li>
+            
+              <li>Id:4,<br/>
+              Name:James Mogaka,<br/>
+              Speciaulty:Optician</li>
+                
+        </card>
+      </div>
+  </div>
   )};
 export default Appointment;
